2dc3eb1b726d9e960c0a414f1770076d1614c08f
chris <lebeaugose72@gmail.com>
2021-03-23 10:08:27 +0100

    ...

---
+++ 02_programming/09_Reboot_day_2/67b37950898621de8b6bcccaab1e63bed4ffa11b/01_reboot_ecommerce/src/eCommerce/index.js
@@ -0,0 +1,14 @@
+const readline = require("readline");
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+//ON BOSSE ICI
+
+// Displaying main menu
+
+console.log("RUBEN'S E-SHOP");
+console.log("1 - Buy a product\n2 - Show cart\n3 - Checkout\n4 - Quit");
+
