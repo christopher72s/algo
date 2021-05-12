f325a36955cb8d5ed6d5bc6e03a87d0f4a370f3e
chris <lebeaugose72@gmail.com>
2021-05-12 08:54:17 +0200

    exoo

---
+++ 06_fullstack_web_development/02_typescript_and_hooks/96dbe13786b8a75dfb05ea4a88a3090cf982a951/01_interact_with_apis/src/components/PlatformPage/index.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import type from "../../types/Platform";
+
+const PlatformPage = (): JSX.Element => {
+  const [platform, setplatform] = React.useState<type.Platform[]>([]);
+
+  React.useEffect(() => {
+    fetch("https://videogames-sparta.herokuapp.com/platforms", { headers: { accept: "application/json" } }).then(
+      async (response) => {
+        setplatform(await response.json());
+      },
+    );
+  }, []);
+  console.log(platform);
+
+  return (
+    <div>
+      {platform.map(({ name }) => {
+        return (
+          <div key={name}>
+            <p> {name}!</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+export default PlatformPage;
+
+/* <select class="ice-cream" name="ice-cream">
+    <option value="">Select One …</option>
+    <option value="chocolate">Chocolate</option> */
